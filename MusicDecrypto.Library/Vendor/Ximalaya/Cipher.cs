85e59336923a072cdb9d285c39239f960d7c6ad7
David Xuang <davidxuang@live.com>
2022-11-21 19:36:13 +0800

    Support Ximalaya

---
+++ MusicDecrypto.Library/Vendor/Ximalaya/Cipher.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Numerics;
+using Cysharp.Collections;
+using MusicDecrypto.Library.Numerics;
+
+namespace MusicDecrypto.Library.Vendor.Ximalaya;
+
+internal sealed class Cipher : IDecryptor, IDisposable
+{
+    private readonly NativeMemoryArray<byte> _key;
+    private readonly int _keySize;
+    private readonly ushort[] _map;
+
+    public Cipher(ReadOnlySpan<byte> key, ushort[] map)
+    {
+        _key = SimdHelper.PadCircularly(key);
+        _keySize = key.Length;
+        _map = map;
+    }
+
+    public void Dispose()
+    {
+        _key.Dispose();
+    }
+
+    public void Decrypt(Span<byte> data, long offset)
+    {
+        if (offset == 0)
+        {
+            var buffer = (stackalloc byte[SimdHelper.GetPaddedLength(_map.Length)]);
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                buffer[i] = data[_map[i]];
+            }
+
+            int step = SimdHelper.LaneCount;
+            int i_k;
+            for (int i = 0; i < buffer.Length; i += step)
+            {
+                i_k = (int)((offset + i) % _keySize);
+                var window = buffer[i..(i + step)];
+                var v = new Vector<byte>(window);
+                var k = new Vector<byte>(_key.AsSpan(i_k, step));
+                (v ^ k).CopyTo(window);
+            }
+
+            buffer[.._map.Length].CopyTo(data);
+        }
+    }
+}
---
+++ MusicDecrypto.Library/Vendor/Ximalaya/Decrypto.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using MusicDecrypto.Library.Media;
+
+namespace MusicDecrypto.Library.Vendor.Ximalaya;
+
+internal sealed partial class Decrypto : DecryptoBase
+{
+    private static readonly byte[] _key2 = "xmly"u8.ToArray();
+    private static readonly byte[] _key3 = "3989d111aad5613940f4fc44b639b292"u8.ToArray();
+    private static readonly IDecryptor _decryptor2;
+    private static readonly IDecryptor _decryptor3;
+
+    static Decrypto()
+    {
+        using (var _stream2 = Assembly.GetExecutingAssembly().GetManifestResourceStream("MusicDecrypto.Library.Vendor.Ximalaya.x2m_map.bin")!)
+        {
+            using var _reader2 = new BinaryReader(_stream2);
+            var _map2 = new ushort[(int)(_stream2.Length / 2)];
+            for (int i = 0; i < _map2.Length; i++)
+            {
+                _map2[i] = _reader2.ReadUInt16();
+            }
+            _decryptor2 = new Cipher(_key2, _map2);
+        }
+
+        using (var _stream3 = Assembly.GetExecutingAssembly().GetManifestResourceStream("MusicDecrypto.Library.Vendor.Ximalaya.x3m_map.bin")!)
+        {
+            using var _reader3 = new BinaryReader(_stream3);
+            var _map3 = new ushort[(int)(_stream3.Length / 2)];
+            for (int i = 0; i < _map3.Length; i++)
+            {
+                _map3[i] = _reader3.ReadUInt16();
+            }
+            _decryptor3 = new Cipher(_key3, _map3);
+        }
+    }
+
+    protected override IDecryptor Decryptor { get; init; }
+
+    public Decrypto(MarshalMemoryStream buffer, string name, WarnHandler? warn, AudioTypes type = AudioTypes.Undefined)
+        : base(buffer, name, warn, type)
+    {
+        Decryptor = Path.GetExtension(name) switch
+        {
+            ".x2m" => _decryptor2,
+            ".x3m" => _decryptor3,
+            _      => throw new NotSupportedException()
+        };
+    }
+}
